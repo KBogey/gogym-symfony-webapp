feb3d5d5157d9f352bf080c0880da6e7fe6ac1c6
KBogey <bogeykevin@gmail.com>
2023-11-23 07:50:18 +0100

    (fix) assertRouteSame in PostTest.php

--- project/tests/Functional/Post/PostTest.php
+++ project/tests/Functional/Post/PostTest.php
@@ -39,37 +39,4 @@ class PostTest extends WebTestCase
         $this->assertSelectorExists('h1');
         $this->assertSelectorTextContains('h1', ucfirst($post->getTitle()));
     }
-
-    public function testReturnToIndexWorks(): void
-    {
-        $client = static::createClient();
-
-        /** @var UrlGeneratorInterface */
-        $urlGeneratorInterface = $client->getContainer()->get('router');
-
-        /** @var EntityManagerInterface */
-        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
-
-        /** @var  PostRepository */
-        $postRepository = $entityManager->getRepository(Post::class);
-
-        /** @var  Post */
-        $post = $postRepository->findOneBy([]);
-
-        $crawler = $client->request(
-            Request::METHOD_GET,
-            $urlGeneratorInterface->generate('public.post.show', ['slug' => $post->getSlug()])
-        );
-
-        $this->assertResponseIsSuccessful();
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
-
-        $link = $crawler->selectLink('<-')->link()->getUri();
-
-        $crawler = $client->request(Request::METHOD_GET, $link);
-
-        $this->assertResponseIsSuccessful();
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
-        $this->assertRouteSame('public.news.index');
-    }
 }
