1a3644a4455621c3ba659d87fd37c49afaf67c30
KBogey <bogeykevin@gmail.com>
2023-11-23 08:19:53 +0100

    (test) add a share on twitter/facebook test in PostTest.php

--- project/tests/Functional/Post/PostTest.php
+++ project/tests/Functional/Post/PostTest.php
@@ -39,4 +39,83 @@ class PostTest extends WebTestCase
         $this->assertSelectorExists('h1');
         $this->assertSelectorTextContains('h1', ucfirst($post->getTitle()));
     }
+
+    public function testShareOnFacebookWorks(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+
+        /** @var EntityManagerInterface */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var  PostRepository */
+        $postRepository = $entityManager->getRepository(Post::class);
+
+        /** @var  Post */
+        $post = $postRepository->findOneBy([]);
+
+        $postLink = $urlGeneratorInterface->generate('public.post.show', ['slug' => $post->getSlug()]);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $postLink
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $link = $crawler->filter('.share.facebook')->link()->getUri();
+
+        $this->assertStringContainsString(
+            'https://www.facebook.com/sharer/sharer.php',
+            $link
+        );
+
+        $this->assertStringContainsString(
+            $postLink,
+            $link
+        );
+    }
+
+    public function testShareOnTwitterWorks(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+
+        /** @var EntityManagerInterface */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var  PostRepository */
+        $postRepository = $entityManager->getRepository(Post::class);
+
+        /** @var  Post */
+        $post = $postRepository->findOneBy([]);
+
+        $postLink = $urlGeneratorInterface->generate('public.post.show', ['slug' => $post->getSlug()]);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $postLink
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $link = $crawler->filter('.share.twitter')->link()->getUri();
+
+        $this->assertStringContainsString(
+            'https://twitter.com/intent/tweet',
+            $link
+        );
+
+        $this->assertStringContainsString(
+            $postLink,
+            $link
+        );
+    }
+
 }
